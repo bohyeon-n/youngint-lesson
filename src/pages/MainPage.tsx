57f8201e653413b91701fede362877a727afe769
koo <goosee115@gmail.com>
2019-01-10 16:52:51 +0900

    import 이후 빈칸 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { TrianglePattern } from "../components/TrianglePattern";
 import { Hello } from "../components/Hello";
+
 export default class MainPage extends React.Component {
   render() {
     return (
