00e7f846e944218a57bc60fe3c90d1a400821721
koo <goosee115@gmail.com>
2019-01-14 15:31:04 +0900

    Message Component 추가

---
+++ src/components/Message.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export interface ErrorProps {
+  message: string;
+}
+export class Message extends React.Component<ErrorProps, {}> {
+  render() {
+    const { message } = this.props;
+    return <div>{message}</div>;
+  }
+}
