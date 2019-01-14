c6e52832de7707ac7208fa9d70a7795037df7472
koo <goosee115@gmail.com>
2019-01-14 15:32:25 +0900

    Pattern Container Component 수정

--- src/containers/PatternCC.tsx
+++ src/containers/PatternCC.tsx
@@ -1,12 +1,10 @@
 import * as React from "react";
-import { Input } from "../components/Form";
 import { TrianglePattern } from "../components/TrianglePattern";
 
 export class PatternCC extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Input />
         <TrianglePattern number={5} pattern={"*"} />
       </React.Fragment>
     );
