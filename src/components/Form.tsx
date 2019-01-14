b84de8c79fed44983bfcb60ae53a8891583d8ffd
koo <goosee115@gmail.com>
2019-01-14 15:30:29 +0900

    Form Container 추가

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,33 @@
 import * as React from "react";
 
-export class Input extends React.Component {
+export interface FormProps {
+  value: string;
+  handleChange: any;
+  handleSubmit: any;
+}
+export class Form extends React.Component<FormProps, {}> {
   render() {
+    const { value, handleChange, handleSubmit } = this.props;
     return (
       <React.Fragment>
-        <form>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+        >
           <label>
             Number:
-            <input type="text" name="number" />
+            <input
+              type="text"
+              name="number"
+              value={value}
+              onChange={e => {
+                handleChange(e.target.value);
+              }}
+            />
           </label>
+          <button>Submit</button>
         </form>
       </React.Fragment>
     );
