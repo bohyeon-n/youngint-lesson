4b6aea961f30051bb9ee3d700c2e2821ee2de4c6
koo <goosee115@gmail.com>
2019-01-14 15:33:39 +0900

    Main Page 수정 Form 상태에 따른 TrianglePattern 컴포넌트 삽입으로 변경

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,29 @@
 import * as React from "react";
-import { PatternCC } from "../containers/PatternCC";
-import { Hello } from "../components/Hello";
+// import { PatternCC } from "../containers/PatternCC";
 
+import { Hello } from "../components/Hello";
+import { FormCC } from "../containers/FormCC";
+import { TrianglePattern } from "../components/TrianglePattern";
 export default class MainPage extends React.Component {
+  state = {
+    submitted: false,
+    value: ""
+  };
+  drawPattern = (n: number) => {
+    this.setState({
+      submitted: true,
+      value: n
+    });
+  };
   render() {
+    const { value, submitted } = this.state;
     return (
       <div>
         <Hello name="world" />
-        <PatternCC />
+        <FormCC drawPattern={this.drawPattern} />
+        {submitted ? (
+          <TrianglePattern number={parseFloat(value)} pattern={"*"} />
+        ) : null}
       </div>
     );
   }
