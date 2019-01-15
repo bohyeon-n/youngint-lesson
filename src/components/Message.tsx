15218ffa92e0b01095ca04e20d3c446a8eaea8cd
koo <goosee115@gmail.com>
2019-01-15 10:46:20 +0900

    줄간격 수정

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 export interface ErrorProps {
   message: string;
 }
+
 export class Message extends React.Component<ErrorProps, {}> {
   render() {
     const { message } = this.props;
