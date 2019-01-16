94f211a473c0370a16fff82b7634e843dfdf874f
koo <goosee115@gmail.com>
2019-01-16 16:43:45 +0900

    sass file 추가, sass loader 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import MainPage from "./pages/MainPage";
-
+import "./index.scss";
 ReactDOM.render(<MainPage />, document.getElementById("root"));
