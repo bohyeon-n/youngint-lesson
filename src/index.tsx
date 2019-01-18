54910ae912555bbdd099e42fe1cda185e571b6f1
koo <goosee115@gmail.com>
2019-01-18 14:05:06 +0900

    fontawesome 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,9 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import MainPage from "./pages/MainPage";
 import "./index.scss";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faIgloo } from "@fortawesome/free-solid-svg-icons";
 
+library.add(faIgloo);
 ReactDOM.render(<MainPage />, document.getElementById("root"));
