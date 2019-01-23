5ef74ad2ef8748603fcfa392f90c796b077b88e6
koo <goosee115@gmail.com>
2019-01-23 11:18:43 +0900

    isValid 함수 수정 input value를 string 과 number 값으로 변환 후 valid 체크 테스트 코드 작성시 들어온 값이 number인 경우를 위해 수정함

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,35 +1,36 @@
-const isValid = (numberInputValue: string, pattern: string) => {
-  const number = Number(numberInputValue);
+const isValid = (inputValue: string, pattern: string) => {
+  const inputNumber = Number(inputValue);
+  const inputString = String(inputValue);
   let valid: boolean = false;
   let message: string;
-  if (numberInputValue === "") {
+  if (inputString === "") {
     message = "";
-  } else if (isNaN(number)) {
+  } else if (isNaN(inputNumber)) {
     message = "숫자를 입력해주세요.";
-  } else if (numberInputValue.indexOf(".") !== -1) {
+  } else if (inputString.indexOf(".") !== -1) {
     message = "정수만 입력할 수 있습니다.";
-  } else if (!isFinite(number)) {
+  } else if (!isFinite(inputNumber)) {
     const messageContent =
-      numberInputValue.slice(0, 1) === "-" ? "작은 수" : "큰 수";
+      inputString.slice(0, 1) === "-" ? "작은 수" : "큰 수";
     const messageTemplate = `너무 ${messageContent}를 입력하셨네요🤮 0보다 크고 100보다 작은 정수만 입력할 수 있습니다.`;
     message = messageTemplate;
   } else {
     if (
-      number > 0 &&
-      number <= 100 &&
-      !(pattern === "diamond" && number % 2 === 0)
+      inputNumber > 0 &&
+      inputNumber <= 100 &&
+      !(pattern === "diamond" && inputNumber % 2 === 0)
     ) {
       message = "";
       valid = true;
     } else {
       message =
-        number < 0
+        inputNumber < 0
           ? "0보다 큰 숫자를 입력해주세요."
-          : number > 100
+          : inputNumber > 100
           ? "100보다 큰 숫자는 입력할 수 없습니다."
-          : number === 0
+          : inputNumber === 0
           ? "0은 입력할 수 없습니다."
-          : pattern === "diamond" && number % 2 === 0
+          : pattern === "diamond" && inputNumber % 2 === 0
           ? "다이아몬드 패턴은 홀수만 입력할 수 있습니다"
           : "";
     }
