e08eef0d2f4f22883db398cdf0ba670678dc8348
koo <goosee115@gmail.com>
2019-01-25 13:23:12 +0900

    테스트 코드 추가 ReverseTriangle, Pattern4, Pattern5 테스트 코드 추가함

---
+++ src/modules/Pattern4Patter.test.ts
@@ -0,0 +1,14 @@
+import Pattern4ShapePattern from "../modules/Pattern4ShapePattern";
+
+test("diamond", () => {
+  // arrange
+  let pattern = new Pattern4ShapePattern(3, "*");
+
+  // act
+  let result = pattern.draw();
+
+  // assert
+  expect(result[0]).toBe("  ***");
+  expect(result[1]).toBe(" *** ");
+  expect(result[2]).toBe("***  ");
+});
---
+++ src/modules/Pattern5Pattern.test.ts
@@ -0,0 +1,14 @@
+import Pattern5ShapePattern from "../modules/Pattern5ShapePattern";
+
+test("diamond", () => {
+  // arrange
+  let pattern = new Pattern5ShapePattern(3, "*");
+
+  // act
+  let result = pattern.draw();
+
+  // assert
+  expect(result[0]).toBe("  ***");
+  expect(result[1]).toBe(" **  ");
+  expect(result[2]).toBe("*   ");
+});
