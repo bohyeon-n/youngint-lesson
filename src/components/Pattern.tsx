f5a5fb913811aff4e2f0e39120a6c57c2c9c2c31
koo <goosee115@gmail.com>
2019-01-25 11:58:12 +0900

    Pattern 컴포넌트에 pattern class 로 변경된 사항 적용함

--- src/components/Pattern.tsx
+++ src/components/Pattern.tsx
@@ -1,6 +1,11 @@
 import * as React from "react";
 
-import { createPattern } from "../utils/drawPattern";
+import Patterns from "../utils/Patterns";
+import TriangleShapePattern from "../modules/TriangleShapePattern";
+import DiamondShapePattern from "../modules/DiamondShapePattern";
+import ReverseTrianglePattern from "../modules/ReverseTrianglePattern";
+import Pattern4ShapePattern from "../modules/Pattern4ShapePattern";
+import Pattern5ShapePattern from "../modules/Pattern5ShapePattern";
 
 export interface PatternProps {
   readonly number: number;
@@ -11,7 +16,24 @@ export interface PatternProps {
 export class Pattern extends React.Component<PatternProps, {}> {
   render() {
     const { number, shape, pattern } = this.props;
-    const patterns = createPattern(number, shape, pattern);
+    let patterns: Array<string> = [];
+    switch (pattern) {
+      case Patterns.Triangle:
+        patterns = new TriangleShapePattern(number, shape).draw();
+        break;
+      case Patterns.ReverseTriangle:
+        patterns = new ReverseTrianglePattern(number, shape).draw();
+        break;
+      case Patterns.Diamond:
+        patterns = new DiamondShapePattern(number, shape).draw();
+        break;
+      case Patterns.Pattern4:
+        patterns = new Pattern4ShapePattern(number, shape).draw();
+        break;
+      case Patterns.Pattern5:
+        patterns = new Pattern5ShapePattern(number, shape).draw();
+        break;
+    }
 
     return (
       <div
