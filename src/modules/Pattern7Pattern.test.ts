02c5213517f639e860b27c9d429049382b6f0da3
koo <goosee115@gmail.com>
2019-01-29 17:55:16 +0900

    pattern7 테스트 코드 추가

---
+++ src/modules/Pattern7Pattern.test.ts
@@ -0,0 +1,15 @@
+import Pattern7ShapePattern from "./Pattern7ShapePattern";
+
+test("pattern7", () => {
+  //arrange
+  let pattern = new Pattern7ShapePattern(3, "*");
+
+  //act
+  let result = pattern.draw();
+
+  // assert
+
+  expect(result[0]).toBe("*       *       *       ");
+  expect(result[1]).toBe("        **      **      ");
+  expect(result[2]).toBe("                ***     ");
+});
