f023f2a1e7019f7dde39aae944a7952498769824
koo <goosee115@gmail.com>
2019-01-30 15:45:20 +0900

    pattern7Random  패턴 추가

---
+++ src/modules/Pattern7RandomShapePattern.ts
@@ -0,0 +1,44 @@
+import BaseShapePattern from "./BaseShapePattern";
+import Patterns from "../utils/Patterns";
+
+export default class Pattern7RandomShapePattern extends BaseShapePattern {
+  constructor(totalNumber: number, shape: string) {
+    super(totalNumber, shape);
+  }
+
+  protected createPattern = (pattern: string) => {
+    const number = this.totalNumber;
+    const shapeArray = new Array(number).fill("");
+    let patterns: Array<any> = [];
+    const blankLine = this.repeatCharacterNumberTimes(" ", number);
+    shapeArray.map((content, index) => {
+      const line = new Array(index + 1).fill("").map((content, index) => {
+        return this.drawPatternLine(index + 1, Patterns.Pattern7Random, index);
+      });
+
+      patterns.push(line);
+    });
+
+    const shuffledPatterns = this.shuffleArray(patterns);
+
+    const horizontalDirection = shapeArray.map((content, lineIndex) => {
+      return shapeArray.map((content, index) => {
+        return shuffledPatterns[lineIndex][index]
+          ? shuffledPatterns[lineIndex][index]
+          : blankLine;
+      });
+    });
+
+    return horizontalDirection.map((line, i) => {
+      let joinedLine = "";
+      line.map((shape, index) => {
+        joinedLine += horizontalDirection[index][i];
+      });
+      return joinedLine;
+    });
+  };
+
+  draw = () => {
+    return this.createPattern(Patterns.Pattern7);
+  };
+}
