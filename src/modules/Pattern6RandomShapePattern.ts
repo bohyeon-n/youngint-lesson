88b57638de16791d227f301e9f5882350a772300
koo <goosee115@gmail.com>
2019-01-30 15:44:40 +0900

    Pattern6Random 패턴 추가

---
+++ src/modules/Pattern6RandomShapePattern.ts
@@ -0,0 +1,42 @@
+import BaseShapePattern from "./BaseShapePattern";
+import Patterns from "../utils/Patterns";
+
+export default class Pattern6RandomShapePattern extends BaseShapePattern {
+  constructor(totalNumber: number, shape: string) {
+    super(totalNumber, shape);
+  }
+
+  protected countShapesInLines = () => {
+    const { totalNumber } = this;
+    let array: any = [];
+    new Array(totalNumber)
+      .fill("")
+      .map((i, index) => index + 1)
+      .map(lineNumber => {
+        array.push([
+          ...new Array(lineNumber).fill("").map((line, index) => index)
+        ]);
+      });
+
+    return this.shuffleArray(array);
+  };
+
+  protected createPattern = (pattern: string) => {
+    const number = this.totalNumber;
+    const shapeArray = this.countShapesInLines();
+    const patterns: Array<string> = [];
+
+    shapeArray.map((section: number[], index: number) => {
+      section.map((shapeCount: number, index: number) => {
+        patterns.push(this.drawPatternLine(shapeCount + 1, pattern, index));
+      });
+      patterns.push(this.repeatCharacterNumberTimes(" ", number));
+    });
+
+    return patterns;
+  };
+
+  draw = () => {
+    return this.createPattern(Patterns.Pattern6);
+  };
+}
