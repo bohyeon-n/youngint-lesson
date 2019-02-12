7208682c5b91a44de84330915e810983880c0175
koo <goosee115@gmail.com>
2019-02-12 11:04:13 +0900

    BaseShapePattern 클래스 주석 추가

--- src/modules/BaseShapePattern.ts
+++ src/modules/BaseShapePattern.ts
@@ -16,6 +16,7 @@ abstract class BaseShapePattern {
     this.direction = "vertical";
   }
 
+  //  한 라인의 공백을 세기 위한 메서드 index는 전체 패턴에서 몇 번째 라인인지를 표시합니다.
   protected countBlanksInLine = (shapeCount: number, index: number): number => {
     const { totalNumber } = this;
     return totalNumber - shapeCount;
@@ -28,6 +29,7 @@ abstract class BaseShapePattern {
     return character.repeat(number);
   };
 
+  // 전체 패턴에서 한 라인 당 몇 개의 모양이 있는지 세기 위한 메서드(패턴의 모든 라인을 셉니다.)
   protected countShapesInLines = (): number[] => {
     const { totalNumber } = this;
     return new Array(totalNumber).fill("").map((i, index) => index);
@@ -63,7 +65,6 @@ abstract class BaseShapePattern {
     const patterns = shapeArray.map((shapeCount, index) =>
       this.createLine(shapeCount + 1, index)
     );
-
     const patternModel = new PatternModel(patterns);
     this.patternSetModel.addModel(patternModel);
   };
